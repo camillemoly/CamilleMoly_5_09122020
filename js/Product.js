0fd601709e903e8324d57e421aac67e99a3bd1ed
Camille Moly <camille.moly@gmail.com>
2020-12-16 10:17:27 +0100

    feat: create file class Product

---
+++ js/Product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(imageUrl, name, price, quantity) {
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
+export { Product };

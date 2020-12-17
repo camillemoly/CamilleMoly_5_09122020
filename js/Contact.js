fdcdde5aa1195c3503384e8b927bff6ca4de9144
Camille Moly <camille.moly@gmail.com>
2020-12-17 16:55:46 +0100

    add Contact Class file

---
+++ js/Contact.js
@@ -0,0 +1,11 @@
+class Contact {
+    constructor(firstName, lastName, address, city, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+    }
+}
+
+export { Contact };

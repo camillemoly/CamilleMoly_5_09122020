cca7c0dc224091c64bbc0d0af78a91716012acf0
Camille Moly <camille.moly@gmail.com>
2020-12-27 16:01:38 +0100

    create file with valid form function

---
+++ js/validation-form.js
@@ -0,0 +1,21 @@
+function nameValid(value) { // function which checks if the value of form input doesn't contain numbers
+    let regex = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+    return regex.test(value);
+}
+
+function emailValid(value) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(value);
+}
+
+function specialCaractersValid(value) {
+    let regex = /^[^@&"()!_$*€£`+=\/;?#]+$/;
+    return regex.test(value);
+}
+
+function onlyWhitespacesInvalid(value) {
+    let regex = /^\s+$/;
+    return regex.test(value);
+}
+
+export { nameValid, emailValid, specialCaractersValid, onlyWhitespacesInvalid }; 
